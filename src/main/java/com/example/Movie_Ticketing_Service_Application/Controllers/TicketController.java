0f882e8573056155cea558ed2d9edf492dadbce5
ShubhKrishna15 <Krsnavasudey@gmail.com>
2023-06-30 05:25:18 +0530

    ticketApi

--- src/main/java/com/example/Movie_Ticketing_Service_Application/Controllers/TicketController.java
+++ src/main/java/com/example/Movie_Ticketing_Service_Application/Controllers/TicketController.java
@@ -2,6 +2,8 @@ package com.example.Movie_Ticketing_Service_Application.Controllers;
 
 
 import com.example.Movie_Ticketing_Service_Application.EntryDTOs.TicketEntryDTO;
+import com.example.Movie_Ticketing_Service_Application.Services.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/ticket")
 public class TicketController {
-
+    @Autowired
+    TicketService ticketService;
 
     @PostMapping("/book")
     public String bookTicket(@RequestBody TicketEntryDTO ticketEntryDTO){
+            try{
+                    String  result  = ticketService.addTicket(ticketEntryDTO);
+                    return result;
 
+            }catch(Exception e){
+
+                return "Error";
+
+
+            }
 
     }
 }
--- src/main/java/com/example/Movie_Ticketing_Service_Application/Services/TicketService.java
+++ src/main/java/com/example/Movie_Ticketing_Service_Application/Services/TicketService.java
@@ -6,10 +6,13 @@ import com.example.Movie_Ticketing_Service_Application.EntryDTOs.TicketEntryDTO;
 import com.example.Movie_Ticketing_Service_Application.Models.Show;
 import com.example.Movie_Ticketing_Service_Application.Models.ShowSeat;
 import com.example.Movie_Ticketing_Service_Application.Models.Ticket;
+import com.example.Movie_Ticketing_Service_Application.Models.User;
 import com.example.Movie_Ticketing_Service_Application.Repositories.ShowRepository;
+import com.example.Movie_Ticketing_Service_Application.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -17,14 +20,69 @@ public class TicketService {
 
     @Autowired
     ShowRepository showRepository;
-    public String bookTicket(TicketEntryDTO ticketEntryDTO){
+
+    @Autowired
+    UserRepository userRepository;
+
+    int totalAmount  = 0;
+    public String addTicket(TicketEntryDTO ticketEntryDTO) throws Exception{
 
         Ticket ticket = TicketEntryConverter.covertDTO(ticketEntryDTO);
+
+        int showId = ticketEntryDTO.getShowId();
+        Show show = showRepository.findById(showId).get();
+
+        List<String> requestedSeats = ticketEntryDTO.getRequestedSeats();
 
 
         //checking for validity weather the mentioned seat are available or not
 
         Boolean checkValidity = areSeatsValid(ticketEntryDTO);
+        if(checkValidity==false){
+            throw new Exception("Requested seats are already booked");
+        }
+
+        ticket.setTotalAmount(totalAmount);
+
+        String allotedSeats = getAllotedSeatsFromRequestedSeatList(requestedSeats);
+        ticket.setBookedSeats(allotedSeats);
+
+        //Setting the other key Attribute
+        ticket.setMovieName(show.getMovie().getMovieName());
+        ticket.setShowDate(show.getShowDate());
+        ticket.setShowTime(show.getShowTime());
+        ticket.setTheaterName(show.getTheater().getName());
+
+        // Setting the Mapped attributes
+        User user = userRepository.findById(ticketEntryDTO.getUserId()).get();
+        ticket.setUser(user);
+        ticket.setShow(show);
+
+        //Setting the parents
+        List<Ticket>ticketList = user.getBookedTickets();
+        ticketList.add(ticket);
+        user.setBookedTickets(ticketList);
+
+        List<Ticket> ticketList1 = show.getListOfBookedTickets();
+        ticketList1.add(ticket);
+        show.setListOfBookedTickets(ticketList1);
+
+        showRepository.save(show);
+        userRepository.save(user);
+
+
+        return "Your tickets are successfully Booked" ;
+
+    }
+    private String getAllotedSeatsFromRequestedSeatList(List<String>requestedSeats){
+        String result = "" ;
+        for(String seat : requestedSeats){
+
+            result = result + seat+", ";
+
+
+        }
+        return result;
     }
 
     private Boolean areSeatsValid(TicketEntryDTO ticketEntryDTO ){
@@ -35,7 +93,23 @@ public class TicketService {
         int showId = ticketEntryDTO.getShowId();
         Show show = showRepository.findById(showId).get();
 
+
         List<ShowSeat> showSeatList = show.getShowSeatList();
+
+       for(ShowSeat showSeat : showSeatList ){
+           String seatNumber = showSeat.getSeatNo();
+           if(ListOfRequestedSeats.contains(seatNumber)){
+               if(showSeat.isBooked()){
+                   return false;
+               }
+               totalAmount += showSeat.getPrice();
+               showSeat.setBooked(true);
+               showSeat.setBookedAt(new Date());
+           }
+
+       }
+
+       return true;
 
 
 
